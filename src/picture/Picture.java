1129650c75e817bc42b57726e4f4f55ffc8b8935
Артём <malagrius@gmail.com>
2020-02-20 13:56:00 +0300

    Frame is Visible

--- src/picture/Picture.java
+++ src/picture/Picture.java
@@ -15,7 +15,8 @@ public class Picture {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Frame frame = new Frame();
+        frame.setVisible(true);
     }
     
 }
